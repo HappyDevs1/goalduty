fa442f5905fcc27f28705f002a18fec986f6d4f9
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-05-12 19:57:13 +0200

    fix: :bug: fixing errors related to wrong controller usage

--- backend/app/routes/daily_report_route.py
+++ backend/app/routes/daily_report_route.py
@@ -1,14 +1,14 @@
 from flask import Blueprint
-from ..controllers.daily_tasks_controller import (
-    get_all_daily_tasks,
-    get_daily_task_by_id,
-    create_daily_task,
-    update_daily_task
+from ..controllers.daily_report_controller import (
+    get_all_daily_reports,
+    get_daily_report_by_id,
+    create_daily_report,
+    update_daily_report
 )
 
-daily_tasks_bp = Blueprint('daily_tasks_bp', __name__)
+daily_tasks_bp = Blueprint('daily_report_bp', __name__)
 
-daily_tasks_bp.route('/daily_tasks', methods=['GET'])(get_all_daily_tasks)
-daily_tasks_bp.route('/daily_tasks/<int:daily_task_id>', methods=['GET'])(get_daily_task_by_id)
-daily_tasks_bp.route('/daily_tasks', methods=['POST'])(create_daily_task)
-daily_tasks_bp.route('/daily_tasks/<int:daily_task_id>', methods=['PUT'])(update_daily_task)
+daily_tasks_bp.route('/daily_report', methods=['GET'])(get_all_daily_reports)
+daily_tasks_bp.route('/daily_report/<int:daily_report_id>', methods=['GET'])(get_daily_report_by_id)
+daily_tasks_bp.route('/daily_report', methods=['POST'])(create_daily_report)
+daily_tasks_bp.route('/daily_report/<int:daily_report_id>', methods=['PUT'])(update_daily_report)
