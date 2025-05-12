bf7b4b9da5d47a4069b1b388e048f875e647fdd1
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-05-12 20:47:51 +0200

    fix: :bug: fixing typo in the bp

--- backend/app/routes/daily_report_route.py
+++ backend/app/routes/daily_report_route.py
@@ -6,9 +6,9 @@ from ..controllers.daily_report_controller import (
     update_daily_report
 )
 
-daily_tasks_bp = Blueprint('daily_report_bp', __name__)
+daily_report_bp = Blueprint('daily_report_bp', __name__)
 
-daily_tasks_bp.route('/daily_report', methods=['GET'])(get_all_daily_reports)
-daily_tasks_bp.route('/daily_report/<int:daily_report_id>', methods=['GET'])(get_daily_report_by_id)
-daily_tasks_bp.route('/daily_report', methods=['POST'])(create_daily_report)
-daily_tasks_bp.route('/daily_report/<int:daily_report_id>', methods=['PUT'])(update_daily_report)
+daily_report_bp.route('/daily_report', methods=['GET'])(get_all_daily_reports)
+daily_report_bp.route('/daily_report/<int:daily_report_id>', methods=['GET'])(get_daily_report_by_id)
+daily_report_bp.route('/daily_report', methods=['POST'])(create_daily_report)
+daily_report_bp.route('/daily_report/<int:daily_report_id>', methods=['PUT'])(update_daily_report)
