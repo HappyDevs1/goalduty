f59b04e1e8c4cb3b8f41e357aaaf2df72b7eca7e
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-05-12 20:49:43 +0200

    fix: :pencil2: fixing capital letter in Integet instead of integer which causes errors

--- backend/app/models/daily_tasks_model.py
+++ backend/app/models/daily_tasks_model.py
@@ -1,7 +1,7 @@
 from ..db import db
 
 class DailyTask(db.Model):
-  id = db.Column(db.integer, primary_key=True)
+  id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(100), nullable=False)
   completed = db.Column(db.Boolean, default=False)
   user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
