7ec20ae61f7bc6527dce4e9fd06528f96043f7d5
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-05-12 22:04:33 +0200

    feat: :hammer: adding missing controller function to get daily task by user

--- backend/app/routes/daily_tasks_route.py
+++ backend/app/routes/daily_tasks_route.py
@@ -3,7 +3,8 @@ from ..controllers.daily_tasks_controller import (
     get_all_daily_tasks,
     get_daily_task_by_id,
     create_daily_task,
-    update_daily_task
+    update_daily_task,
+    get_daily_tasks_by_user
 )
 
 daily_tasks_bp = Blueprint('daily_tasks_bp', __name__)
@@ -12,3 +13,4 @@ daily_tasks_bp.route('/daily_tasks', methods=['GET'])(get_all_daily_tasks)
 daily_tasks_bp.route('/daily_tasks/<int:daily_task_id>', methods=['GET'])(get_daily_task_by_id)
 daily_tasks_bp.route('/daily_tasks', methods=['POST'])(create_daily_task)
 daily_tasks_bp.route('/daily_tasks/<int:daily_task_id>', methods=['PUT'])(update_daily_task)
+daily_tasks_bp.route('/daily_tasks/user/<int:user_id>', methods=['GET'])(get_daily_tasks_by_user)
