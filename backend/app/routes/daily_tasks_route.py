87b33fe6144562c0b5e3f89462b50a9ac98d03b9
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-05-12 17:08:21 +0200

    feat: :sparkles: daily task route to interact with the daily task controller

---
+++ backend/app/routes/daily_tasks_route.py
@@ -0,0 +1,14 @@
+from flask import Blueprint
+from ..controllers.daily_tasks_controller import (
+    get_all_daily_tasks,
+    get_daily_task_by_id,
+    create_daily_task,
+    update_daily_task
+)
+
+daily_tasks_bp = Blueprint('daily_tasks_bp', __name__)
+
+daily_tasks_bp.route('/daily_tasks', methods=['GET'])(get_all_daily_tasks)
+daily_tasks_bp.route('/daily_tasks/<int:daily_task_id>', methods=['GET'])(get_daily_task_by_id)
+daily_tasks_bp.route('/daily_tasks', methods=['POST'])(create_daily_task)
+daily_tasks_bp.route('/daily_tasks/<int:daily_task_id>', methods=['PUT'])(update_daily_task)
