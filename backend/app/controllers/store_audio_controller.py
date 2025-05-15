ee0a5e81848e150baee37692f79025534f22a4e9
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-05-15 02:22:18 +0200

    fix: :bug: fixing audio upload errors and final working version for audio upload testing

--- backend/app/controllers/store_audio_controller.py
+++ backend/app/controllers/store_audio_controller.py
@@ -12,27 +12,27 @@ superbase = create_client(SUPABASE_URL, SERVICE_SUPABASE_KEY)
 
 # Production ready function to upload audio files to Supabase storage
 def upload_to_storage(file_obj, filename):
-  bucket = superbase.storage().from_("goalduty-audio")
+  bucket = superbase.storage.from_("goalduty-audio")
   bucket.upload(filename, file_obj)
   public_url = f"{SUPABASE_URL}/storage/v1/object/public/audio_uploads/{filename}"
   return public_url
 
-def store_audio():
-  if 'file' not in request.files:
-    return jsonify({'message': 'No file part'}), 400
+# def store_audio():
+#   if 'file' not in request.files:
+#     return jsonify({'message': 'No file part'}), 400
 
-  file = request.files['file']
+#   file = request.files['file']
 
-  if file.filename == '':
-    return jsonify({'message': 'No selected file'}), 400
+#   if file.filename == '':
+#     return jsonify({'message': 'No selected file'}), 400
 
-  filename = file.filename
-  public_url = upload_to_storage(file, filename)
+#   filename = file.filename
+#   public_url = upload_to_storage(file, filename)
 
-  return jsonify({
-    'message': 'File uploaded successfully',
-    'url': public_url
-  }), 200
+#   return jsonify({
+#     'message': 'File uploaded successfully',
+#     'url': public_url
+#   }), 200
 
 # Test function to upload audio files to Supabase storage
 def store_audio():
