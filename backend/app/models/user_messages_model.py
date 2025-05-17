d84acaca05f6adf82925d47e6d16ff12fca3b482
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-05-18 00:13:22 +0200

    refactor: :recycle: updating user messages to also use a json column to store messages rather than each message being stored in its own column which makes it hard during ai intergration to generate future goals

--- backend/app/models/user_messages_model.py
+++ backend/app/models/user_messages_model.py
@@ -2,9 +2,5 @@ from ..db import db
 
 class UserMessages(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  user_first_message = db.Column(db.Text, nullable=False)
-  user_second_message = db.Column(db.Text, nullable=False)
-  user_third_message = db.Column(db.Text, nullable=False)
-  user_fourth_message = db.Column(db.Text, nullable=False)
-  user_fifth_message = db.Column(db.Text, nullable=False)
-  user_id = db.Column(db.Integer, db.ForegnKey('user.id'), nullable=False)
+  messages = db.Column(db.JSON, nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
