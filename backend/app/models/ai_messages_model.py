0f8165609b88781a63609e93aca7a7fe6cdfcc3f
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-05-18 00:01:17 +0200

    refactor: :recycle: refactoring to storing ai messages in a column of json rather than having its own column which is really unnecessary and complicate the ai goal task generation

--- backend/app/models/ai_messages_model.py
+++ backend/app/models/ai_messages_model.py
@@ -2,9 +2,5 @@ from ..db import db
 
 class AIMessages(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  first_question = db.Column(db.Text, nullable=False)
-  second_question = db.Column(db.Text)
-  third_question = db.Column(db.Text)
-  fourth_question = db.Column(db.Text)
-  fifth_question = db.Column(db.Text)
-  user_id = db.Column(db.Integer, db.ForegnKey('user.id'), nullable=False)
+  messages = db.Column(db.JSON, nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
