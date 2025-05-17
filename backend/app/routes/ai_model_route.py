88b2ca1dab4d169b9ccc378b0cb67211dbb5fd81
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-05-18 00:34:51 +0200

    fix: :bug: adding routes to interact with the api

--- backend/app/routes/ai_model_route.py
+++ backend/app/routes/ai_model_route.py
@@ -1,23 +1,10 @@
 from flask import request, Blueprint, jsonify
-from ..ai_models.text_to_speech_model import (transcribe)
-from io import BytesIO
+from ..ai_models.text_to_speech_model import transcribe
+from ..ai_models.conversational_model import get_followup
+from ..ai_models.summary_model import get_summary
 
 ai_bp = Blueprint('ai_bp', __name__)
 
 ai_bp.route('/transcribe', methods=['POST'])(transcribe)
-
-# Production ready flask function
-
-# @ai_bp.route('/transcribe', methods=['POST'])
-# def transcribe():
-#     if 'file' not in request.files:
-#         return jsonify({'error': 'No file provided'}), 400
-
-#     file = request.files['file']
-
-#     try:
-#         audio_bytes = BytesIO(file.read())
-#         result = transcribe_audio_to_text(audio_bytes)
-#         return jsonify({'text': result}), 200
-#     except Exception as e:
-#         return jsonify({'error': str(e)}), 500
+ai_bp.route('/chat', methods=['POST'])(get_followup)
+ai_bp.route('/summarize', methods=['POST'])(get_summary)
