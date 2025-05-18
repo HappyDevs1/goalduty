3aeca0b3a83062fdd6ce92b25eedc4c6fe5ff931
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-05-18 15:46:53 +0200

    refactor: :recycle: updating the string character to enable storing longer paragraphs

--- backend/app/models/future_goals_model.py
+++ backend/app/models/future_goals_model.py
@@ -2,5 +2,5 @@ from ..db import db
 
 class FutureGoal(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(100), nullable=False)
+  name = db.Column(db.String(500), nullable=False)
   user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
