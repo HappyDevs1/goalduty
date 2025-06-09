46936432ebb86c8e9cf8150215efa46e2ffbbb85
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-06-09 08:52:10 +0200

    feat: :sparkles: adding functional feedback page with demo text

--- frontend/src/app/feedback/page.tsx
+++ frontend/src/app/feedback/page.tsx
@@ -1,8 +1,8 @@
 export default function Feedback () {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold mb-4">Feedback</h1>
-      <p className="text-lg">This is the feedback page</p>
+    <div className="flex flex-col my-5">
+      <h1 className="text-2xl font-bold mb-4">Feedback</h1>
+      <p className="tex-sm text-gray-700 max-w-95">This is the feedback page, I will add text that makes sense later on but this is still a demo text, okay?</p>
     </div>
   );
 }
