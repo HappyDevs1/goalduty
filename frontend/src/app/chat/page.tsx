fd355a9753f26fa23d79e6215d0630a94b91f4fb
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-06-13 22:45:51 +0200

    feat: :sparkles: using the chat component in the chat page

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,8 +1,12 @@
+"use client";
+
+import ChatComponent from "../components/ChatComponent";
+
 export default function ChatPage() {
   return (
-    <div>
-      <h1>Chat Page</h1>
-      <p>This is the chat page.</p>
+    <div className="flex flex-col h-screen justify-end px-4 py-2 bg-gray-100">
+      <h1 className="text-xl font-semibold mb-4">Chat Page</h1>
+      <ChatComponent />
     </div>
   );
 }
