cf05a60dec335d5f76cf591f44089cce6c460d6c
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-06-14 17:25:51 +0200

    feat: :sparkles: adding password field to the user fields

--- backend/app/models/user_model.py
+++ backend/app/models/user_model.py
@@ -4,6 +4,7 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(50), nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(200), nullable=False)
   daily_tasks = db.relationship('DailyTask', backref='user', lazy=True)
   daily_reports = db.relationship('DailyReport', backref='user', lazy=True)
   future_goals = db.relationship('FutureGoal', backref='user', lazy=True)
