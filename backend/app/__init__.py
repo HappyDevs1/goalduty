8f75b25f1abad25219f655ef35e8281b28121cbd
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-06-14 18:11:05 +0200

    fix: :bug: allowing cors to enable requests from any location

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,11 +2,15 @@ from flask import Flask
 from .db import db
 from dotenv import load_dotenv
 import os
+from flask_cors import CORS
 
 load_dotenv()
 
 def create_app():
   app = Flask(__name__)
+
+  CORS(app)
+
   app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
