347fa5c4ea39f7217c4dd557f91f40b849cad3e2
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-06-14 15:01:36 +0200

    feat: :sparkles: user services to connect the frontend with the backend

---
+++ frontend/src/app/services/user.ts
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { localBaseUrl } from "../utils/serverUrl";
+
+const API_BASE_URL = `${localBaseUrl}/users`;
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const registerUser = async (userData: any) => {
+  try {
+    const response = await api.post(`${API_BASE_URL}/users`, userData);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating user:", error);
+    throw error;
+  }
+};
+
+export const getUser = async (userId: string) => {
+  try {
+    const response = await api.get(`/users/${userId}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    throw error;
+  }
+}
+
+export const updateUser = async (userId: string, userData: any) => {
+  try {
+    const response = await api.put(`/users/${userId}`, userData);
+    return response.data;
+  } catch (error) {
+    console.error("Error updating user:", error);
+    throw error;
+  }
+};
+
+export const deleteUser = async (userId: string) => {
+  try {
+    const response = await api.delete(`/users/${userId}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    throw error;
+  }
+};
+
+export const getAllUsers = async () => {
+  try {
+    const response = await api.get("/users");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching all users:", error);
+    throw error;
+  }
+};
