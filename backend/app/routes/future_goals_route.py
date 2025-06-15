93f8d51a86b2899fbc86049ba3de19fea23eec63
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-06-15 22:15:05 +0200

    feat: :sparkles: adding new route to get future goals by user

--- backend/app/routes/future_goals_route.py
+++ backend/app/routes/future_goals_route.py
@@ -3,7 +3,8 @@ from ..controllers.future_goals_controller import (
     get_all_future_goals,
     get_future_goal_by_id,
     create_future_goal,
-    update_future_goal
+    update_future_goal,
+    get_future_goals_by_user_id
 )
 
 future_goals_bp = Blueprint('future_goals_bp', __name__)
@@ -12,3 +13,4 @@ future_goals_bp.route('/future_goals', methods=['GET'])(get_all_future_goals)
 future_goals_bp.route('/future_goals/<int:future_goals_id>', methods=['GET'])(get_future_goal_by_id)
 future_goals_bp.route('/future_goals', methods=['POST'])(create_future_goal)
 future_goals_bp.route('/future_goals/<int:future_goals_id>', methods=['PUT'])(update_future_goal)
+future_goals_bp.route('/future_goals/user/<int:user_id>', methods=['GET'])(get_future_goals_by_user_id)
